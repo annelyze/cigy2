cc47efa94bd3b75ce5ee10423a2d57d3a48b2bea
Annelies Van Extergem <annelies.vanextergem@gmail.com>
2017-08-10 20:14:38 +0200

    Refactor NPS customer gauge code.

--- src/Frontend/Modules/CigyWidgets/Services/CustomerGauge.php
+++ src/Frontend/Modules/CigyWidgets/Services/CustomerGauge.php
@@ -14,23 +14,6 @@ class CustomerGauge
     const API_LOCATION = 'https://api.customergauge.com/v4.1/';
     const API_KEY = 'sgMBH2x9gKuwkpbfTemzBYQdMSrMya00rNB8P9h6UMCfcNu52RFEQwSCojpWtqWU';
 
-    public function execute(): void
-    {
-        parent::execute();
-        $this->loadTemplate();
-
-        $teamNpsDetail = $this->getNpsDetail(FrontendCigyWidgetsModel::getTeamName($this->pageTeamFilter));
-
-        $companyNpsDetail = $this->getCompanyNpsDetail();
-        $npsData = array(
-            'team_name' => FrontendCigyWidgetsModel::getTeamName($this->pageTeamFilter),
-            'team' => $teamNpsDetail->Data->nps,
-            'company' => $companyNpsDetail->Data->nps,
-            'target' => '9',
-        );
-        $this->template->assign('widgetNps', $npsData);
-    }
-
     private function getHttpHeader(){
         $header = array();
         $header[] = 'Content-length: 0';
@@ -38,30 +21,6 @@ class CustomerGauge
         $header[] = 'Authorization: api_key ' . self::API_KEY;
 
         return $header;
-    }
-
-    private function getCompanyNpsDetail(){
-        return $this->getNpsDetail('');
-    }
-
-    private function getNpsDetail($team){
-        $curl = curl_init();
-        // set URL and other appropriate options
-        $url = $this->customerguageApiLocation . 'report/nps.json?date_range_from=' . date('Y') . '-01-01';
-        if($team != '')
-        {
-            $url = $url.'&filter_SegmentB=' . $team . '';
-        }
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, self::getHttpHeader());
-
-        $npsDetail = json_decode(curl_exec($curl));
-
-        // close cURL resource, and free up system resources
-        curl_close($curl);
-
-        return $npsDetail;
     }
 
     /**
